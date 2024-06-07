e2aef29b14256cf46c2bfa38a450ed3e7d9eaa94
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-06-07 16:18:33 +0800

    Back Ground & Slight Changes

--- Assets/pipe_spawn.cs
+++ Assets/pipe_spawn.cs
@@ -32,7 +32,7 @@ public class pipe_spawn : MonoBehaviour
 
     void spawnPipe ()
     {
-        float lowestPoint = transform.position.y - heightOffset;
+        float lowestPoint = transform.position.y - heightOffset + 10;
         float highestPoint = transform.position.y + heightOffset;
 
         Instantiate ( pipe, new Vector3 ( transform.position.x, Random.Range ( lowestPoint, highestPoint ), 0 ), transform.rotation );
